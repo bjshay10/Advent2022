9f3dc3ef58e75b5f753141adf548e2ad386524ee
bjshay10 <bj.shay@d51schools.org>
2022-12-08 12:46:56 -0700

    Day 6 part 2

--- Day6/6_2.js
+++ Day6/6_2.js
@@ -1,13 +1,46 @@
 var fs = require('fs')
 
+var marker = ''
+var markerPosition = 0
+var toCheck = ''
+var isMarker = 'N'
 
-fs.readFile('.\\Day6\\input-test.txt', function(err, data){
-//fs.readFile('.\\Day6\\input.txt', function(err, data){
+function checkIfMarker(str) {
+    let chars = str.split('')
+    //console.log(chars)
+    let uniqueChars = [...new Set(chars)]
+    //console.log(uniqueChars)
+    if (uniqueChars.length === 14) {
+        return 'Y'
+    }
+}
+
+//fs.readFile('.\\Day6\\input-test.txt', function(err, data){
+fs.readFile('.\\Day6\\input.txt', function(err, data){
     if(err) throw err
 
     console.log('STARTING')
 
-    
+    //loop through character string
+    //find first substring of 4 that has no repeating letter
+    //return the length of string to the end of that string
 
-    console.log(`COMPLETE`)
+    //console.log(data.toString())
+
+    for(i=0; i<data.length-13; i++){
+        toCheck = data.toString().substring(i,i+14)
+        //console.log(toCheck)
+        isMarker = checkIfMarker(toCheck)
+
+        if (isMarker === 'Y'){
+            marker = toCheck
+            markerPosition = i+14
+            break
+        }
+        toCheck=''
+        markerPostion=0
+    }
+
+    // test is looking for JPQM
+    console.log(`COMPLETE marker is ${marker} at ${markerPosition}`)
 })
