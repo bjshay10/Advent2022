f15260071f0e406d45029deb56dba780b0183e16
bjshay10 <bj.shay@d51schools.org>
2022-12-13 08:32:17 -0700

    day 10

---
+++ Day10/10_1.js
@@ -0,0 +1,10 @@
+var fs = require('fs')
+
+fs.readFile('.\\Day10\\input-test.txt', function(err, data){
+//fs.readFile('.\\Day10\\input.txt', function(err, data){
+    if(err) throw err
+
+    console.log('STARTING')
+
+    console.log(`COMPLETE`)
+})
