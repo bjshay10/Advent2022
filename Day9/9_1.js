ff4cf182d19ce03e5d43ff3c55c235a1d385d0ce
bjshay10 <bj.shay@d51schools.org>
2022-12-13 08:30:53 -0700

    day 9 initial

---
+++ Day9/9_1.js
@@ -0,0 +1,12 @@
+var fs = require('fs')
+
+fs.readFile('.\\Day9\\input-test.txt', function(err, data){
+//fs.readFile('.\\Day9\\input.txt', function(err, data){
+    if(err) throw err
+
+    console.log('STARTING')
+
+
+
+    console.log(`COMPLETE`)
+})
